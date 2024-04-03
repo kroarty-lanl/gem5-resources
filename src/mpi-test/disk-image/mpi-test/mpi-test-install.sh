ab344ac4b7450323084ec740bbbb326ef28831b4
Kyle Roarty <kroarty@lanl.gov>
2024-04-03 12:16:22 -0600

    resources: Build MPI from source

--- src/mpi-test/disk-image/mpi-test/mpi-test-install.sh
+++ src/mpi-test/disk-image/mpi-test/mpi-test-install.sh
@@ -1,16 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install build-essential \
-                        openmpi-bin libopenmpi-dev \
+                        wget \
                         python3 gdb
 
-cd mpi-tests
+wget https://download.open-mpi.org/release/open-mpi/v5.0/openmpi-5.0.2.tar.bz2
 
-mpicc send_recv.c -o send_recv_c
-mpicc hello_mpi.c -g -o hello_mpi_c
+tar xf openmpi-5.0.2.tar.bz2
+cd openmpi-5.0.2
 
-mpif90 send_recv.f90 -o send_recv_f90
-mpif90 hello_mpi.f90 -o hello_mpi_f90
+./configure  --enable-mpi-fortran=no CFLAGS="-mno-avx512f -mno-avx" CXXCFLAGS="-mno-avx512f -mno-avx" 2>&1 | tee config.out
+make -j 4 all 2>&1 | tee make.out
+sudo make install 2>&1 | tee install.out
 
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+
+cd ~/mpi-tests
+
+mpicc send_recv.c -g -mno-avx512f -mno-avx -o send_recv_c
+mpicc hello_mpi.c -g -mno-avx512f -mno-avx -o hello_mpi_c
+
+#mpif90 send_recv.f90 -g -mno-avx -mno-avx512f -o send_recv_f90
+#mpif90 hello_mpi.f90 -g -mno-avx -mno-avx512f -o hello_mpi_f90
+
+
+mpirun -n 2 ./hello_mpi_c
+mpirun -n 2 ./send_recv_c
